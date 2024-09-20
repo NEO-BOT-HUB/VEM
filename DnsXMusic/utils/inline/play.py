db3d5abd9c32fbc0e99d64dd0b05a61053e5cab2
Official Durgesh <durgeshgokhale656@gmail.com>
2024-09-20 18:52:32 +0530

    Auto Fixed

--- DnsXMusic/utils/inline/play.py
+++ DnsXMusic/utils/inline/play.py
@@ -20,27 +20,27 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
     percentage = (played_sec / duration_sec) * 100
     umm = math.floor(percentage)
     if 0 <= umm <= 10:
-    bar = "▱▱▱▱▱▱▱▱▱▱"
-elif 10 < umm <= 20:
-    bar = "▰▱▱▱▱▱▱▱▱▱"
-elif 20 < umm <= 30:
-    bar = "▰▰▱▱▱▱▱▱▱▱"
-elif 30 <= umm < 40:
-    bar = "▰▰▰▱▱▱▱▱▱▱"
-elif 40 <= umm < 50:
-    bar = "▰▰▰▰▱▱▱▱▱▱"
-elif 50 <= umm < 60:
-    bar = "▰▰▰▰▰▱▱▱▱▱"
-elif 60 <= umm < 70:
-    bar = "▰▰▰▰▰▰▱▱▱▱"
-elif 70 <= umm < 80:
-    bar = "▰▰▰▰▰▰▰▱▱▱"
-elif 80 <= umm < 90:
-    bar = "▰▰▰▰▰▰▰▰▱▱"
-elif 90 <= umm < 100:
-    bar = "▰▰▰▰▰▰▰▰▰▱"
-else:
-    bar = "▰▰▰▰▰▰▰▰▰▰"
+        bar = "▱▱▱▱▱▱▱▱▱▱"
+    elif 10 < umm <= 20:
+        bar = "▰▱▱▱▱▱▱▱▱▱"
+    elif 20 < umm <= 30:
+        bar = "▰▰▱▱▱▱▱▱▱▱"
+    elif 30 <= umm < 40:
+        bar = "▰▰▰▱▱▱▱▱▱▱"
+    elif 40 <= umm < 50:
+        bar = "▰▰▰▰▱▱▱▱▱▱"
+    elif 50 <= umm < 60:
+        bar = "▰▰▰▰▰▱▱▱▱▱"
+    elif 60 <= umm < 70:
+        bar = "▰▰▰▰▰▰▱▱▱▱"
+    elif 70 <= umm < 80:
+        bar = "▰▰▰▰▰▰▰▱▱▱"
+    elif 80 <= umm < 90:
+        bar = "▰▰▰▰▰▰▰▰▱▱"
+    elif 90 <= umm < 100:
+        bar = "▰▰▰▰▰▰▰▰▰▱"
+    else:
+        bar = "▰▰▰▰▰▰▰▰▰▰"
     buttons = [
         [
             InlineKeyboardButton(
@@ -87,28 +87,28 @@ def telegram_markup_timer(_, chat_id, played, dur):
     duration_sec = time_to_seconds(dur)
     percentage = (played_sec / duration_sec) * 100
     umm = math.floor(percentage)
-   if 0 <= umm <= 10:
-    bar = "▱▱▱▱▱▱▱▱▱▱"
-elif 10 < umm <= 20:
-    bar = "▰▱▱▱▱▱▱▱▱▱"
-elif 20 < umm <= 30:
-    bar = "▰▰▱▱▱▱▱▱▱▱"
-elif 30 <= umm < 40:
-    bar = "▰▰▰▱▱▱▱▱▱▱"
-elif 40 <= umm < 50:
-    bar = "▰▰▰▰▱▱▱▱▱▱"
-elif 50 <= umm < 60:
-    bar = "▰▰▰▰▰▱▱▱▱▱"
-elif 60 <= umm < 70:
-    bar = "▰▰▰▰▰▰▱▱▱▱"
-elif 70 <= umm < 80:
-    bar = "▰▰▰▰▰▰▰▱▱▱"
-elif 80 <= umm < 90:
-    bar = "▰▰▰▰▰▰▰▰▱▱"
-elif 90 <= umm < 100:
-    bar = "▰▰▰▰▰▰▰▰▰▱"
-else:
-    bar = "▰▰▰▰▰▰▰▰▰▰"
+    if 0 <= umm <= 10:
+        bar = "▱▱▱▱▱▱▱▱▱▱"
+    elif 10 < umm <= 20:
+        bar = "▰▱▱▱▱▱▱▱▱▱"
+    elif 20 < umm <= 30:
+        bar = "▰▰▱▱▱▱▱▱▱▱"
+    elif 30 <= umm < 40:
+        bar = "▰▰▰▱▱▱▱▱▱▱"
+    elif 40 <= umm < 50:
+        bar = "▰▰▰▰▱▱▱▱▱▱"
+    elif 50 <= umm < 60:
+        bar = "▰▰▰▰▰▱▱▱▱▱"
+    elif 60 <= umm < 70:
+        bar = "▰▰▰▰▰▰▱▱▱▱"
+    elif 70 <= umm < 80:
+        bar = "▰▰▰▰▰▰▰▱▱▱"
+    elif 80 <= umm < 90:
+        bar = "▰▰▰▰▰▰▰▰▱▱"
+    elif 90 <= umm < 100:
+        bar = "▰▰▰▰▰▰▰▰▰▱"
+    else:
+        bar = "▰▰▰▰▰▰▰▰▰▰"
     buttons = [
         [
             InlineKeyboardButton(
