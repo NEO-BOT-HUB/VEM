049d75b4604522e4e0271cde6e872ed892ac6135
Official Durgesh <durgeshgokhale656@gmail.com>
2024-10-04 19:37:39 +0530

    Auto Fixes

--- DnsXMusic/plugins/tools/Upscale.py
+++ DnsXMusic/plugins/tools/Upscale.py
@@ -1,33 +1,38 @@
+import requests
 from io import BytesIO
 from pyrogram import filters
 from DnsXMusic import app
-import cv2
-import numpy as np
 
-# Upscale image using OpenCV
-def upscale_image(image_data, scale_factor=4):
-    img = cv2.imdecode(np.frombuffer(image_data, np.uint8), cv2.IMREAD_COLOR)
-    upscaled_img = cv2.resize(img, None, fx=scale_factor, fy=scale_factor, interpolation=cv2.INTER_CUBIC)
-    success, encoded_img = cv2.imencode('.jpg', upscaled_img)
-    return encoded_img.tobytes() if success else None
 
-@app.on_message(filters.command("upscale") & filters.reply)
+def upscale_image_api(image_data):
+    files = {
+        'image': image_data
+    }
+    
+    # Send the image to the UP_API for upscaling
+    response = requests.post(UP_API, files=files)
+    
+    if response.status_code == 200:
+        return response.content  # Upscaled image in bytes
+    else:
+        return None
+
+@app.on_message(filters.command(["upscale","p"], prefixes=["/", "!", ".", "U", "u"]) & filters.reply)
 async def upscale_command(client, message):
-    # Ensure the command is a reply to an image
     if message.reply_to_message and message.reply_to_message.photo:
         # Get the largest available image
         photo = message.reply_to_message.photo[-1]
         
         # Download the image
         file = await client.download_media(photo.file_id)
-
-        # Upscale the image
-        upscaled_image = upscale_image(file)
+        
+        # Send the image to the UP_API for upscaling
+        upscaled_image = upscale_image_api(BytesIO(file))
         
         if upscaled_image:
             # Send the upscaled image back to the user
             await message.reply_photo(photo=BytesIO(upscaled_image), caption="Here's your upscaled image!")
         else:
-            await message.reply_text("Sorry, I couldn't upscale the image.")
+            await message.reply_text("Sorry, the image could not be upscaled.")
     else:
         await message.reply_text("Please reply to an image with the /upscale command to use this feature.")
