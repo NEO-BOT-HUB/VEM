f5c003c3144b2b869889cf4d25fa5c439209d604
Official Durgesh <durgeshgokhale656@gmail.com>
2024-10-04 16:22:43 +0530

    Auto Fixes

--- DnsXMusic/plugins/tools/Upscale.py
+++ DnsXMusic/plugins/tools/Upscale.py
@@ -14,16 +14,21 @@ def upscale_image(image_data, scale_factor=4):
 
 @app.on_message(filters.command(["upscale", "pscale", "p"], prefixes=["/", "!", ".", "U", "u"]) & filters.reply)
 async def upscale_command(client, message):
+    # Check if the command is a reply to an image
     if message.reply_to_message and message.reply_to_message.photo:
         # Get the image file ID from the replied message
-        photo = message.reply_to_message.photo[-1]
-        file = await client.download_media(photo.file_id)
+        photo = message.reply_to_message.photo  # This is already a list of photos
+        if photo:  # Check if the photo list is not empty
+            largest_photo = photo[-1]  # Get the largest available resolution
+            file = await client.download_media(largest_photo.file_id)
 
-        # Send the image to be upscaled
-        upscaled_image = upscale_image(file)
-        if upscaled_image:
-            await message.reply_photo(photo=io.BytesIO(upscaled_image), caption="Here's your upscaled image!")
+            # Send the image to be upscaled
+            upscaled_image = upscale_image(file)
+            if upscaled_image:
+                await message.reply_photo(photo=io.BytesIO(upscaled_image), caption="Here's your upscaled image!")
+            else:
+                await message.reply_text("Sorry, I couldn't upscale the image.")
         else:
-            await message.reply_text("Sorry, I couldn't upscale the image.")
+            await message.reply_text("No photo found to upscale.")
     else:
         await message.reply_text("Please reply to an image with the /upscale command to use this feature.")
