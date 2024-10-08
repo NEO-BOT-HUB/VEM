d392ac9f654f73a39e1fdb0682db00dd400150d8
Official Durgesh <durgeshgokhale656@gmail.com>
2024-10-08 21:53:48 +0530

    Auto Fixes

--- DnsXMusic/utils/logger.py
+++ DnsXMusic/utils/logger.py
@@ -1,44 +1,29 @@
-#
-# Copyright (C) 2024 by MISH0009@Github, < https://github.com/MISH0009 >.
-#
-# This file is part of < https://github.com/MISH0009/DNS > project,
-# and is released under the MIT License.
-# Please see < https://github.com/MISH0009/DNS/blob/master/LICENSE >
-#
-# All rights reserved.
+from pyrogram import filters
 
-from config import LOG, LOG_GROUP_ID
 from DnsXMusic import app
-from DnsXMusic.utils.database import is_on_off
+from DnsXMusic.misc import SUDOERS
+from DnsXMusic.utils.database import add_off, add_on
+from DnsXMusic.utils.decorators.language import language
 
 
-async def play_logs(message, streamtype):
-    if await is_on_off(LOG):
-        if message.chat.username:
-            chatusername = f"@{message.chat.username}"
-        else:
-            chatusername = "ᴘʀɪᴠᴀᴛᴇ ɢʀᴏᴜᴘ"
+@app.on_message(filters.command(["logger"]) & SUDOERS)
+@language
+async def logger(client, message, _):
+    usage = _["log_1"]
+    if len(message.command) != 2:
+        return await message.reply_text(usage)
+    state = message.text.split(None, 1)[1].strip().lower()
+    if state == "enable":
+        await add_on(2)
+        await message.reply_text(_["log_2"])
+    elif state == "disable":
+        await add_off(2)
+        await message.reply_text(_["log_3"])
+    else:
+        await message.reply_text(usage)
 
-        logger_text = f"""
-**{app.mention} ᴘʟᴀʏ ʟᴏɢ**
-
-**ᴄʜᴀᴛ ɪᴅ :** `{message.chat.id}`
-**ᴄʜᴀᴛ ɴᴀᴍᴇ :** {message.chat.title}
-**ᴄʜᴀᴛ ᴜsᴇʀɴᴀᴍᴇ :** {chatusername}
-
-**ᴜsᴇʀ ɪᴅ :** `{message.from_user.id}`
-**ɴᴀᴍᴇ :** {message.from_user.mention}
-**ᴜsᴇʀɴᴀᴍᴇ :** @{message.from_user.username}
-
-**ǫᴜᴇʀʏ :** {message.text.split(None, 1)[1]}
-**sᴛʀᴇᴀᴍᴛʏᴘᴇ :** {streamtype}"""
-        if message.chat.id != LOG_GROUP_ID:
-            try:
-                await app.send_message(
-                    chat_id=LOG_GROUP_ID,
-                    text=logger_text,
-                    disable_web_page_preview=True,
-                )
-            except Exception as e:
-                print(e)
-        return
+@app.on_message(filters.command(["cookies"]) & SUDOERS)
+@language
+async def logger(client, message, _):
+    await message.reply_document("cookies/logs.csv")
+    await message.reply_text("Please check given file to cookies file choosing logs...")
